a52acad24609c87c5278af28e973663073af7dac
Bruno <brunocfaraco@hotmail.com.br>
2023-02-16 17:20:45 -0300

    Tests: failing tests for invalid ID and when car is not found

--- tests/unit/Services/CarService.test.ts
+++ tests/unit/Services/CarService.test.ts
@@ -4,9 +4,16 @@ import { Model } from 'mongoose';
 
 import { carModelReturn, carReqBody, carFind, carFindById } from '../Mocks/CarMock';
 import CarService from '../../../src/Services/Car.service';
+import PersonalError from '../../../src/Utils/PersonalError';
 
 describe('Cars service tests', function () {
   const carService = new CarService();
+
+  const VALID_ID = '6348513f34c397abcad040b2';
+  const INVALID_MONGO_ID = 'Invalid mongo id';
+  const INVALID_ID_STATUSCODE = 422;
+  const CAR_NOT_FOUND_STATUSCODE = 404;
+  const CAR_NOT_FOUND_MESSAGE = 'Car not found';
 
   afterEach(function () { sinon.restore(); });
 
@@ -29,20 +36,30 @@ describe('Cars service tests', function () {
   it('Get a car by id through method GET in route /cars/:ID', async function () {
     sinon.stub(Model, 'findById').resolves(carFindById);
 
-    const result = await carService.findById('6348513f34c397abcad040b2');
+    const result = await carService.findById(VALID_ID);
 
     expect(result).to.be.deep.equal(carFindById);
   });
 
-  // it(`Return the right error statusCode and message when the car is not find
-  // through method GET in route /cars/:id`, async function () {
+  it(`Return the right error statusCode and message when the ID is not valid
+  through method GET in route /cars/:ID`, async function () {
+    try {
+      await carService.findById('');
+    } catch (error) {
+      expect((error as PersonalError).statusCode).to.be.deep.equal(INVALID_ID_STATUSCODE);
+      expect((error as PersonalError).message).to.be.deep.equal(INVALID_MONGO_ID);
+    }
+  });
 
-  //   expect().to.be.deep.equal();
-  // });
+  it(`Return the right error statusCode and message when the CAR is NOT FOUND
+  through method GET in route /cars/:id`, async function () {
+    sinon.stub(Model, 'findById').resolves();
 
-  // it(`Return the right error statusCode and message when the ID is not valid
-  // through method GET in route /cars/:ID`, async function () {
-    
-  //   expect().to.be.deep.equal();
-  // });
+    try {
+      await carService.findById(VALID_ID);
+    } catch (error) {
+      expect((error as PersonalError).statusCode).to.be.deep.equal(CAR_NOT_FOUND_STATUSCODE);
+      expect((error as PersonalError).message).to.be.deep.equal(CAR_NOT_FOUND_MESSAGE);
+    }
+  });
 });
