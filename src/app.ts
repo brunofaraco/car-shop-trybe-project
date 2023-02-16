839d95324f11987a72e62f1f791356a1ad1091df
Bruno <brunocfaraco@hotmail.com.br>
2023-02-16 16:18:09 -0300

    Feat: added errorHandler to "app.use()"

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import errorHandler from './Middlewares/Error.middleware';
 import CarRoute from './Routes/Car.routes';
 
 const app = express();
@@ -6,5 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/cars', CarRoute);
+
+app.use(errorHandler);
 
 export default app;
